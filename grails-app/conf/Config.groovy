60bc61350721f459de36edb5652fdc443bff9630
Tomas Lin <tomas@secretescapes.com>
2011-07-29 11:16:19 +0100

    added serverUrl

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -33,3 +33,5 @@ grails.plugin.inviter.google.key='inviter.cloudfoundry.com'
 grails.plugin.inviter.google.secret='mIE86xbAHcH5Cr2J9BYoz0k9'
 grails.plugin.inviter.linkedin.key='k25tupolcvf1'
 grails.plugin.inviter.linkedin.secret='KBtxC418sdmu5eur'
+
+grails.serverURL = 'http://inviter.cloudfoundry.com'
