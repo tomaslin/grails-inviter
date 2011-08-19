44e770375474dbc301436696780246bd30c359a2
Tomas Lin <tomas@secretescapes.com>
2011-08-19 17:47:04 +0100

    inviter

--- InviterGrailsPlugin.groovy
+++ InviterGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class InviterGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [mail:'0.9', rest:'0.6.1' ]
+    def dependsOn = [mail:'0.9 > *', rest:'0.6.1' ]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
