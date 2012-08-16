c2fd357f3b5c8b5dedc1453ab71e63bde3a08ae9
enr <enr@example.com>
2012-08-16 15:41:12 +0200

    check for provider enabled and new view for sent invites

--- grails-app/controllers/grails/plugin/inviter/InviterController.groovy
+++ grails-app/controllers/grails/plugin/inviter/InviterController.groovy
@@ -9,39 +9,27 @@ class InviterController {
 	def index = { }
 
 	def invite() {
-        /*
-		def service = resolveService(params.provider)
-		def authInfo = service.getAuthDetails(createLink(action: 'contacts', controller: 'inviter', absolute: 'true', params: [provider: params.provider]))
-		if (authInfo.requestToken)
-		{
-			session["${params.provider}_requestToken"] = authInfo.requestToken
-		}
-		redirect(url: authInfo.authUrl)
-        */
+        validateRequest(params.provider)
         redirect(url: getAuthUrl(params.provider, 'contacts'))
 	}
 
 	def pick() {
-        /*
-		def service = resolveService(params.provider)
-
-		def authInfo = service.getAuthDetails(createLink(action: 'pickContacts', controller: 'inviter', absolute: 'true', params: [provider: params.provider]))
-
-		if (authInfo.requestToken)
-		{
-			session["${params.provider}_requestToken"] = authInfo.requestToken
-		}
-        */
+        validateRequest(params.provider)
 		redirect(url: getAuthUrl(params.provider, 'pickContacts'))
 	}
     
+    def validateRequest(provider) {
+        if (    !provider ||
+                !grailsApplication.config.grails.plugin.inviter[params.provider] || 
+                !grailsApplication.config.grails.plugin.inviter[params.provider].enabled) {
+                throw new RuntimeException("Provider '${provider}' not enabled")
+        }
+    }
+    
     private getAuthUrl(provider, action) {
 		def service = resolveService(provider)
-
 		def authInfo = service.getAuthDetails(createLink(action: action, controller: 'inviter', absolute: 'true', params: [provider: provider]))
-
-		if (authInfo.requestToken)
-		{
+		if (authInfo.requestToken) {
 			session["${provider}_requestToken"] = authInfo.requestToken
 		}
         return authInfo.authUrl
@@ -53,7 +41,6 @@ class InviterController {
 		def message = params.message + ' ' + ( grailsApplication.config.grails.plugin.inviter.defaultMessage ?: '' ) as String
 
 		if( grailsApplication.config.grails.plugin.inviter.debug ){
-
 			render """
 
 				<html>
@@ -65,7 +52,6 @@ class InviterController {
 
 			"""
 			return
-
 		} else {
 			if (service.useEmail) {
 				params.addresses.split(',').each { address ->
@@ -78,17 +64,18 @@ class InviterController {
 			} else {
 				params.addresses.split(',').each { address ->
 					def response = service.sendMessage( accessToken: accessToken, link: params.link, message: params.message, description: params.description, contact:address, subject: params.subject )
-
-					render response
-					return
-
+                    log.info "response = ${response}"
 				}
 			}
 		}
-		render "Your messages have been sent. See <a href=\"${request.contextPath}/greenmail\">report</a>"
+        render(view: '/inviter/sent', model: [addresses:params.addresses, success:true], plugin: 'inviter')
 	}
 
+    /*
+     * the action intended to show the 'send invites' form.
+     */
 	def contacts() {
+        validateRequest(params.provider)
 		def service = resolveService(params.provider)
 		def accessToken = service.getAccessToken(params, session["${params.provider}_requestToken"])
 		session["${params.provider}_accessToken"] = accessToken
@@ -96,7 +83,11 @@ class InviterController {
 		render(view: '/inviter/contacts', model: [contacts: contacts, provider: params.provider], plugin: 'inviter')
 	}
 
+    /*
+     * the action intended to show the 'pick contacts' form.
+     */
 	def pickContacts() {
+        validateRequest(params.provider)
 		def service = resolveService(params.provider)
 		def accessToken = service.getAccessToken(params, session["${params.provider}_requestToken"])
 		session["${params.provider}_accessToken"] = accessToken
@@ -123,7 +114,6 @@ class InviterController {
 	}
 
 	private def resolveService(provider) {
-
 		def serviceName = "${ provider as String }InviterService"
 		serviceName = serviceName[0].toString().toLowerCase() + serviceName[1..serviceName.length() - 1]
 		grailsApplication.mainContext.getBean(serviceName)
---
+++ grails-app/views/inviter/sent.gsp
@@ -0,0 +1,21 @@
+<html>
+<head><title>Invites sent</title></head>
+
+<r:require module="inviter"/>
+<r:layoutResources/>
+
+<body>
+
+	<h2>Invites sent</h2>
+    
+    <p>Your messages have been sent.
+    
+    <p>addresses = ${addresses}
+    
+    <p>success = ${success}
+    
+    <p><a href="${request.contextPath}/greenmail">Greenmail plugin report</a>
+
+<r:layoutResources/>
+</body>
+</html>
