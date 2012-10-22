2e3aa363e0f4ff5c20610fcb4edbc4526054ecb2
enr <enrico.comiti@gmail.com>
2012-10-22 14:01:50 +0200

    removed jQuery

--- web-app/js/inviter.js
+++ web-app/js/inviter.js
@@ -69,102 +69,84 @@ if (!Array.prototype.filter)
 }
 
 var inviter = inviter || function() {};
-inviter.prototype.onDomReady = function(callback){ // callback should be a function
+
+inviter.prototype.onDomReady = function(callback){
     if (document.addEventListener) {
-        // If the browser supports the DOMContentLoaded event,
-        // assign the callback function to execute when that event fires
-        document.addEventListener("DOMContentLoaded", callback, false);
+        document.addEventListener('DOMContentLoaded', callback, false);
     } else {
         if(document.body && document.body.lastChild){
-            // If the DOM is available for access, execute the callback function
             callback();
         } else {
-            // Reexecute the current function, denoted by arguments.callee,
-            // after waiting a brief nanosecond so as not to lock up the browser
             return setTimeout(arguments.callee, 0);
         }
     }
 };
 
-inviter.prototype.isNullSafe = function(variable) {
+// isNullSafe
+inviter.prototype.isUsable = function(variable) {
     return (typeof(variable) != 'undefined' && variable != null);
 };
 
+// show friend div. display: inline-block;
+inviter.prototype.show = function(element) {
+    element.style.display = 'inline-block';
+}
+
+inviter.prototype.hide = function(element) {
+    element.style.display = 'none';
+}
+
+inviter.prototype.text = function(element) {
+    var text = document.body.textContent ? 'textContent' : 'innerText';
+    return element[text];
+}
 
 inviter.prototype.getAllFriends = function() {
     var friends = [];
     var friendsNode = document.getElementById('inviter-friends');
+    if (!inviter.isUsable(friendsNode)) { return friends; }
     var els = friendsNode.getElementsByTagName('div');
     var elsLen = els.length;
     for(var i=0; i<elsLen; i++) {
-        //console.log('el '+i);
         friends.push(els[i]);
     }
     return friends;
 }
 
 // adds or removes elements from the list of addresses
-function toggleAddress(address) {
-    console.log('toggle address '+address);
-	//if ($.inArray(address, addresses) >= 0) {
+inviter.prototype.toggleAddress = function(address) {
     var addressIdx = addresses.indexOf(address);
     if (addressIdx >= 0) {
-        console.log('toggle addresses 0 '+addresses);
         addresses.splice(addressIdx, 1);
-        /*
-		addresses = $.grep(addresses, function(val) {
-            console.log('toggle address / grep val '+val);
-			return val != address
-		});
-        */
-        console.log('toggle addresses 1 '+addresses);
-        
-        var els = getByEmail(address);
+        var els = inviter.getByEmail(address);
         var elsLen = els.length;
         for(var i=0; i<elsLen; i++) {
             els[i].className = 'friend';
         }
-        /*
-		getByEmail(address).each(function() {
-			$(this).removeClass('selected')
-		});
-        */
 	} else {
 		addresses.push(address);
-        var els = getByEmail(address);
+        var els = inviter.getByEmail(address);
         var elsLen = els.length;
         for(var i=0; i<elsLen; i++) {
             els[i].className = 'friend selected';
         }
-        /*
-		getByEmail(address).each(function() {
-			$(this).addClass('selected')
-		});
-        */
 	}
-	//$('#addresses').val(addresses.join(','))
     var addressesNode = document.getElementById('addresses');
-    if (inviter.isNullSafe(addressesNode)) {
+    if (inviter.isUsable(addressesNode)) {
         addressesNode.value = addresses.join(',');
     }
 
 }
 
-function getByEmail(address) {
+inviter.prototype.getByEmail = function(address) {
     var friends = inviter.getAllFriends();
-    //console.log('friends = '+friends);
-    // $('.friends div')
-    //if (Array.isArray(friends) {
-    if (inviter.isNullSafe(friends)) {
-        return friends.filter(function(node) {
-            //console.log('friends filter '+node.getAttribute('data-email') + '/'+address);
-            //return $(this).attr('data-email') == address
-            return node.getAttribute('data-email') == address;
-        });
-    }
+    return friends.filter(function(node) {
+        return node.getAttribute('data-email') == address;
+    });
     return [];
 }
 
+// function used to add delay to binding search field.
 var typewatch = function(){
     var timer = 0;
     return function(callback, ms){
@@ -175,136 +157,85 @@ var typewatch = function(){
 
 
 inviter.prototype.bindEvent = function(element, eventType, handler) {
-    console.log('binding '+element+' - '+eventType);
-    if (inviter.isNullSafe(element)) {
+    if (inviter.isUsable(element)) {
         if (typeof element.addEventListener != "undefined") {
             element.addEventListener(eventType, handler, false);
-        //if addEventListener is not present, see if this is an IE browser
         } else if (typeof element.attachEvent != "undefined") {
-            //element.attachEvent('on'+eventType, handler);
             element.attachEvent('on' + eventType, function(){
-                /* use call to simulate addEventListener
-                 * This will make sure the callback gets the element for "this"
-                 * and will ensure the function's first argument is the event object
-                 */
                  handler.call(event.srcElement,event);
             });
         }
     }
 }
 
+inviter.prototype.clearSelection = function() {
+    addresses = [];
+    var friends = inviter.getAllFriends();
+    var elsLen = friends.length;
+    for(var i=0; i<elsLen; i++) {
+        friends[i].className = 'friend';
+    }
+    var addressesNode = document.getElementById('addresses');
+    if (inviter.isUsable(addressesNode)) {
+        addressesNode.value = '';
+    }
+}
+
+inviter.prototype.selectAll = function() {
+    addresses = [];
+    var friends = inviter.getAllFriends();
+    var elsLen = friends.length;
+    for(var i=0; i<elsLen; i++) {
+        var friend = friends[i];
+        addresses.push(friend.getAttribute('data-email'));
+        friend.className = 'friend selected';
+    }
+    var addressesNode = document.getElementById('addresses');
+    if (inviter.isUsable(addressesNode)) {
+        addressesNode.value = addresses.join(',');
+    }
+}
+
 inviter = new inviter();
 
-//$(document).ready(function() {
 inviter.onDomReady(function() {
-    /*
-	$('.friends div').click(function() {
-		toggleAddress($(this).attr('data-email'));
-	});
-    */
     var friends = inviter.getAllFriends();
-    if (inviter.isNullSafe(friends)) {
-        var elsLen = friends.length;
-        for(var i=0; i<elsLen; i++) {
-            var friend = friends[i];
-            inviter.bindEvent(friend, 'click', function(e) {
-                toggleAddress(this.getAttribute('data-email'));
-            });
-        }
+    var filterField = document.getElementById('filterField');
+    if (!inviter.isUsable(friends) || !inviter.isUsable(filterField)) { return; }
+    var friendsSize = friends.length;
+    for(var i=0; i<friendsSize; i++) {
+        var friend = friends[i];
+        inviter.bindEvent(friend, 'click', function(e) {
+            inviter.toggleAddress(this.getAttribute('data-email'));
+        });
     }
-
-    
-    
-    
-    
-    
-    var filterField = document.getElementById('clearSelection');
     inviter.bindEvent(filterField, 'focus', function() {
-        //$('#filterField').focus(function() {
-		if ($('#filterField').val() == INVITER_FILTER_BLANK) {
-			$('#filterField').val('')
-		}
+        if (filterField.value == INVITER_FILTER_BLANK) {
+            filterField.value = '';
+        }
 	});
     inviter.bindEvent(filterField, 'focusout', function() {
-	//$('#filterField').focusout(function() {
-		if ($('#filterField').val() == '') {
-			$('#filterField').val(INVITER_FILTER_BLANK)
-		}
+        if (filterField.value == '') {
+            filterField.value = INVITER_FILTER_BLANK;
+        }
 	});
-    
-	//$('#filterField').bindWithDelay( 'keyup', function() {
     inviter.bindEvent(document.getElementById('filterField'), 'keyup', function () {
         typewatch(function() {
-            var filterVal = $('#filterField').val()
+            var filterVal = filterField.value;
             if (filterVal == '') {
-                $('.friends div').show()
+                for(var i=0; i<friendsSize; i++) {
+                    inviter.show(friends[i]);
+                }
             } else if (filterVal.length > 0) {
                 var rgx = new RegExp(filterVal, "i");
-                $('.friends div').each(function() {
-                    ($(this).text().search(rgx) < 0 && $(this).attr('data-email').search(rgx) < 0) ? $(this).hide() : $(this).show();
-                });
+                for(var i=0; i<friendsSize; i++) {
+                    var friend = friends[i];
+                    (inviter.text(friend).search(rgx) < 0 && friend.getAttribute('data-email').search(rgx) < 0) ? inviter.hide(friend) : inviter.show(friend);
+                }
             }
         }, 500 );
     });
-    
-	$('#filterField').val(INVITER_FILTER_BLANK)
-	//$('#clearSelection').click(clearSelection);
-    inviter.bindEvent(document.getElementById('clearSelection'), 'click', clearSelection);
-    inviter.bindEvent(document.getElementById('selectAll'), 'click', selectAll);
-	//$('#selectAll').click(selectAll);
+    filterField.value = INVITER_FILTER_BLANK;
+    inviter.bindEvent(document.getElementById('clearSelection'), 'click', inviter.clearSelection);
+    inviter.bindEvent(document.getElementById('selectAll'), 'click', inviter.selectAll);
 });
-
-/*
-bindWithDelay jQuery plugin
-Author: Brian Grinstead
-MIT license: http://www.opensource.org/licenses/mit-license.php
-http://github.com/bgrins/bindWithDelay
-http://briangrinstead.com/files/bindWithDelay
-*/
-
-/*
-(function($) {
-$.fn.bindWithDelay = function( type, data, fn, timeout, throttle ) {
-	var wait = null;
-	var that = this;
-
-	if ( $.isFunction( data ) ) {
-		throttle = timeout;
-		timeout = fn;
-		fn = data;
-		data = undefined;
-	}
-
-	function cb() {
-		var e = $.extend(true, { }, arguments[0]);
-		var throttler = function() {
-			wait = null;
-			fn.apply(that, [e]);
-		};
-
-		if (!throttle) { clearTimeout(wait); }
-		if (!throttle || !wait) { wait = setTimeout(throttler, timeout); }
-	}
-
-	return this.bind(type, data, cb);
-}
-})(jQuery);
-*/
-
-function clearSelection() {
-    addresses = [];
-    $('.friends div').each(function() {
-        $(this).removeClass('selected');
-    });
-    $('#addresses').val('')
-}
-
-function selectAll() {
-    addresses = [];
-    $('.friends div').each(function() {
-        addresses.push($(this).attr('data-email'));
-        $(this).addClass('selected');
-    });
-    console.log('select all addresses='+addresses);
-    $('#addresses').val(addresses.join(','))
-}
